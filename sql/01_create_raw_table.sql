f71b319d4b70d1faf68d49ffcb741480f2d098ef
olveraalec <aolvera99999@gmail.com>
2026-07-08 22:03:13 -0700

    Use existing SF crime raw data filename

--- sql/01_create_raw_table.sql
+++ sql/01_create_raw_table.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE TABLE sf_crime_raw AS
 SELECT *
 FROM read_csv_auto(
-    'data/raw/train.csv',
+    'data/raw/san_francisco_crime_train.csv',
     header = true,
     ignore_errors = true
 );
